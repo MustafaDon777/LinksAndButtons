2bf10290dee6c340fd3f55dca095882c0d563ddc
Mustafa Donmez <116388196+MustafaDon777@users.noreply.github.com>
2023-09-10 17:35:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,67 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("data.json")
         .then(response => response.json())
         .then(data => {
-            // Function to update the JSON data with a new entry
-            function addNewEntry(title, url, password) {
-                const newEntry = {
-                    title,
-                    url,
-                    password
-                };
-                data.push(newEntry);
-
-                // Update the JSON file
-                fetch("data.json", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(data)
-                })
-                .then(() => {
-                    // Clear the form fields
-                    document.getElementById("newTitle").value = "";
-                    document.getElementById("newURL").value = "";
-                    document.getElementById("newPassword").value = "";
-                    
-                    // Reload the page to display the updated data
-                    location.reload();
-                })
-                .catch(error => {
-                    console.error("Error updating JSON data:", error);
-                });
-            }
-
             // Iterate through the JSON data and create links with divs
             const linksContainer = document.getElementById("links-container");
 
             data.forEach(item => {
                 const linkDiv = document.createElement("div");
+                linkDiv.classList = "lnButton";
                 const link = document.createElement("a");
                 link.textContent = item.title;
                 link.href = item.url;
                 link.target = "_blank"; // Open links in a new tab
                 
-                // Add a click event listener to the <a> element to copy the password and open the link
+                // Add a click event listener to the <a> element to copy the password
                 link.addEventListener("click", function (event) {
                     event.preventDefault();
                     copyPasswordToClipboard(item.password);
-                    window.open(item.url, "_blank"); // Open the link in a new tab
+                  window.open(item.url, "_blank");
                 });
 
                 linkDiv.appendChild(link);
                 linksContainer.appendChild(linkDiv);
-            });
-
-            // Form submission handling
-            const addEntryForm = document.getElementById("add-entry-form");
-            addEntryForm.addEventListener("submit", function (event) {
-                event.preventDefault();
-                const newTitle = document.getElementById("newTitle").value;
-                const newURL = document.getElementById("newURL").value;
-                const newPassword = document.getElementById("newPassword").value;
-
-                addNewEntry(newTitle, newURL, newPassword);
             });
         })
         .catch(error => {
